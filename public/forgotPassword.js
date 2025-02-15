5e3fdf9822e827c44ec274d48003fb7b5ed6e910
Abhinab Sharma <146541054+Abhinab04@users.noreply.github.com>
2025-02-15 19:05:23 +0530

    Delete public directory

--- public/forgotPassword.js
+++
@@ -1,9 +0,0 @@
-function showError(inputElement, message) {
-    const errorMessage = document.createElement("div");
-    errorMessage.className = "error-message";
-    errorMessage.style.color = "red";
-    errorMessage.style.fontSize = "12px";
-    errorMessage.textContent = message;
-    inputElement.style.borderColor = "red";
-    inputElement.parentElement.insertBefore(errorMessage, inputElement);
-}
